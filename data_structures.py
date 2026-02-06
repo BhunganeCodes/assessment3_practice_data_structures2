99375633ce74e06a75841fb4019369e53e2d7110
BhunganeCodes <thamsanqahadebe98@gmail.com>
2026-02-06 07:01:16 +0200

    tests: practice assessment setup

---
+++ data_structures.py
@@ -0,0 +1,50 @@
+
+# ============================
+# TODO:Question 1
+# ============================
+def split_coords(coordinates: list) -> tuple:
+    pass
+
+
+# ============================
+# TODO:Question 2
+# ============================
+def create_id_lookup(user_data: list) -> dict:
+    pass
+
+
+# ============================
+# TODO:Question 3
+# ============================
+def extract_unique_tags(posts: list) -> set:
+    pass
+
+
+# ============================
+# TODO:Question 4
+# ============================
+def group_by_category(items: list) -> dict:
+    pass
+
+
+
+# ============================
+# TODO:Question 5
+# ============================
+def batch_api_dispatcher(user_ids: list | tuple) -> list:
+    pass
+
+
+# ============================
+# TODO:Question 6
+# ============================
+def social_graph_inverter(following_list: dict) -> dict:
+    pass
+
+
+# ============================
+# TODO:Question 7
+# ============================
+def fibonacci_generator(n: int) -> list:
+    pass
+
